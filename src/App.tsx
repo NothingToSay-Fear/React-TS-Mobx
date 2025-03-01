3ae0762f2f99b83901f74e8accf2f20682eafa54
Fear <1143533573@qq.com>
2025-03-01 19:25:06 +0800

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { newRequest } from "./axios/axios";
 import img1 from "./1.jpg";
 
 const App: FC = observer(() => {
-  var a = 12345
+  var a = 123456
   const [count, setCount] = useState(0);
   const add = () => {
     setCount((count) => count + 1);
